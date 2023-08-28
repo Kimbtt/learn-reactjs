2ae02b7f4a856e4acce962baf70e9166a0365f88
kimbui <buitrongkim214@gmail.com>
2023-08-29 00:20:52 +0900

    Section 9 lession 51

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AlbumFeature from "./features/Album/pages";
 import Counter from "./components/Counter";
 import ColorBox from "./components/ColorBox";
 import TodoFeature from "./features/Todo/components";
+import {Link, NavLink, Route, Switch} from "react-router-dom";
 
 function App() {
   const name = "kim";
@@ -27,7 +28,31 @@ function App() {
       {/*// </div>*/}
 
         <div className="App">
-          <TodoFeature/>
+          Header
+          {/*<p><Link to="/todos">Todo</Link></p>*/}
+          {/*<p><Link to="/albums">Album</Link></p>*/}
+
+          <p><NavLink to="/todos">Todo</NavLink></p>
+          <p><NavLink to="/albums">Album</NavLink></p>
+
+          {/*
+          Sự khác biệt giữa Link và NavLink
+          NavLink sẽ thêm một số thuộc tính vào thẻ như: class="active"
+          */}
+
+          {/*
+          Công dụng của Switch:
+          nó sẽ match lần lượt từ trên xuống dưới nếu cái nào match chính xác đầu tiên thì  sẽ dừng lại
+          Tránh trường hợp bị render nhiều component
+          */}
+          <Switch>
+            <Route path="/" component={TodoFeature}></Route>
+            <Route path="/" component={TodoFeature}></Route>
+            <Route path="/" component={TodoFeature}></Route>
+            <Route path="/todos" component={TodoFeature}></Route>
+            <Route path="/albums" component={AlbumFeature}></Route>
+          </Switch>
+          Footer
         </div>
 
     </>
